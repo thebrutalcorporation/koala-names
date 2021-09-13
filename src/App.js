3e6778083cb26f2fb3e78f57332bcb860a049e2f
Charlie Astrada <carlosastrada@gmail.com>
2021-09-12 21:51:20 -0300

    feat: improve loading speed
    Lazy load paths for Analyzer, About,  and Result routes to improve bundle sizes.  Also add strictmode to react app. use Spinner animation as fallback for loading routes.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
-import { useState } from "react";
+import React, { useState, StrictMode, lazy, Suspense } from "react";
+import { ChakraProvider, Spinner } from "@chakra-ui/react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Characteristics from "./Characteristics";
 import Layout from "./Layout";
-import Analyzer from "./Analyzer";
-import About from "./About";
 import Hero from "./Hero";
-import Result from "./Result";
+
+const Analyzer = lazy(() => import("./Analyzer"));
+const About = lazy(() => import("./About"));
+const Result = lazy(() => import("./Result"));
 
 const App = () => {
   const [gender, setGender] = useState("");
@@ -23,40 +23,47 @@ const App = () => {
 
   return (
     <ChakraProvider>
-      <Router>
-        <Layout reset={reset}>
-          <Switch>
-            <Route path="/analyze">
-              <Analyzer gender={gender} characteristics={characteristics} />
-            </Route>
-            <Route path="/result">
-              <Result gender={gender} characteristics={characteristics} />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/selection">
-              <Characteristics
-                limit={limit}
-                setLimit={setLimit}
-                gender={gender}
-                setGender={setGender}
-                characteristics={characteristics}
-                setCharacteristics={setCharacteristics}
-              />
-            </Route>
-            <Route path="/">
-              <Hero
-                setGender={setGender}
-                title="Don't give your kid stupid names"
-                subtitle="Let our expert laoweis choose one so your kid doesn't get bullied!"
-              />
-            </Route>
-          </Switch>
-        </Layout>
-      </Router>
+      <Suspense fallback={<Spinner />}>
+        <Router>
+          <Layout reset={reset}>
+            <Switch>
+              <Route path="/analyze">
+                <Analyzer gender={gender} characteristics={characteristics} />
+              </Route>
+              <Route path="/result">
+                <Result gender={gender} characteristics={characteristics} />
+              </Route>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/selection">
+                <Characteristics
+                  limit={limit}
+                  setLimit={setLimit}
+                  gender={gender}
+                  setGender={setGender}
+                  characteristics={characteristics}
+                  setCharacteristics={setCharacteristics}
+                />
+              </Route>
+              <Route path="/">
+                <Hero
+                  setGender={setGender}
+                  title="Don't give your kid stupid names"
+                  subtitle="Let our expert laoweis choose one so your kid doesn't get bullied!"
+                />
+              </Route>
+            </Switch>
+          </Layout>
+        </Router>
+      </Suspense>
     </ChakraProvider>
   );
 };
 
-render(<App />, document.getElementById("root"));
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
